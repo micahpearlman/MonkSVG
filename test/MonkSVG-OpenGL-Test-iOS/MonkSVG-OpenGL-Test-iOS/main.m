29eaebe462fcd11a0cb7d1b1859879a61dc4690a
Micah Pearlman <micahpearlman@gmail.com>
2011-06-22 12:52:29 -0700

    added SVG OpenGL example

---
+++ test/MonkSVG-OpenGL-Test-iOS/MonkSVG-OpenGL-Test-iOS/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MonkSVG-OpenGL-Test-iOS
+//
+//  Created by Micah Pearlman on 6/22/11.
+//  Copyright 2011 Zero Vision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+}
