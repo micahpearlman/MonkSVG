38fba64500efe03129db00a8f0cb9a7cc26926f3
Micah Pearlman <micah@zeroengineering.io>
2022-02-24 16:44:10 -0800

    example code readme

--- README.md
+++ README.md
@@ -24,3 +24,59 @@ mkdir build \
     &&  cmake --build . 
 
 ```
+
+## Use
+
+Initialize and load an SVG:
+
+```
+    // Initialize MonkVG using GLES 2.0 rendering
+    vgCreateContextMNK(WINDOW_WIDTH, WINDOW_HEIGHT,
+                        VG_RENDERING_BACKEND_TYPE_OPENGLES20);
+
+
+    // create an OpenVG (MonkVG) handler	
+    MonkSVG::ISVGHandler::SmartPtr svg_handler =  MonkSVG::OpenVG_SVGHandler::create();
+
+    // load an example
+    std::string svgFilePath = "./data/tiger.svg";
+    std::fstream is( svgFilePath.c_str(), std::fstream::in );
+    std::stringstream ss;
+    ss << is.rdbuf();
+
+    // run it through the svg parser
+    MonkSVG::SVG_Parser* svg_parser = MonkSVG::SVG_Parser::create(svg_handler);
+    svg_parser->parse(ss.str());
+
+```
+
+Draw:
+
+```
+    // Clear the screen.
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // set OpenVG matrix
+    vgSeti(VG_MATRIX_MODE, VG_MATRIX_PATH_USER_TO_SURFACE);
+    vgLoadIdentity();
+
+    // draw svg
+    svg_handler->draw();
+
+    // Swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+```
+
+Cleanup:
+
+```
+    // cleanup 
+    svg_handler.reset();
+    MonkSVG::SVG_Parser::destroy(svg_parser);
+
+    // destroy MonkVG
+    vgDestroyContextMNK();
+```
--- examples/glfw_hello_world.cpp
+++ examples/glfw_hello_world.cpp
@@ -64,20 +64,6 @@ int main(int argc, char **argv) {
 	MonkSVG::SVG_Parser* svg_parser = MonkSVG::SVG_Parser::create(svg_handler);
     svg_parser->parse(ss.str());
 
-    // create a paint
-    VGPaint paint;
-
-    paint = vgCreatePaint();
-    vgSetPaint(paint, VG_FILL_PATH);
-    VGfloat color[4] = {0.0f, 1.0f, 0.0f, 1.0f};
-    vgSetParameterfv(paint, VG_PAINT_COLOR, 4, &color[0]);
-
-    // create a simple box path
-    VGPath path;
-    path = vgCreatePath(VG_PATH_FORMAT_STANDARD, VG_PATH_DATATYPE_F, 1, 0, 0, 0,
-                        VG_PATH_CAPABILITY_ALL);
-    vguRect(path, 0.0f, 0.0f, 100.0f, 150.0f);
-
     // Ensure we can capture the escape key being pressed below
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
@@ -96,11 +82,12 @@ int main(int argc, char **argv) {
         // NOTE:  this is not standard OpenVG
         vgPushOrthoCamera(0.0f, (float)width, (float)height, 0.0f, -1.0f, 1.0f);
 
-        /// draw the basic path
+
+        // set OpenVG matrix
         vgSeti(VG_MATRIX_MODE, VG_MATRIX_PATH_USER_TO_SURFACE);
         vgLoadIdentity();
-        // vgTranslate(width / 2, height / 2);
 
+        // draw svg
         svg_handler->draw();
 
         vgPopOrthoCamera();
@@ -113,9 +100,11 @@ int main(int argc, char **argv) {
     while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
            glfwWindowShouldClose(window) == 0);
 
+    // cleanup 
+    svg_handler.reset();
+    MonkSVG::SVG_Parser::destroy(svg_parser);
+
     // destroy MonkVG
-    vgDestroyPath(path);
-    vgDestroyPaint(paint);
     vgDestroyContextMNK();
 
     glfwDestroyWindow(window);
